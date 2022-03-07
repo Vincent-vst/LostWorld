991c1e8d93726cde89b5186740d5d20d2d9a8f7b
Vincent-vst <louise.insomnia@gmail.com>
2022-03-07 17:41:14 +0100

    initial commit

---
+++ correction_tp1.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(){
+	struct addrinfo hints, *res; 
+	int sockfd, confd, error; 
+
+	memset(&hints , 0, sizeof(hints)); 
+	
+	hints.ai_family = AF_INET6; 
+	hints.ai_socktype = SOCK_STREAM; 
+	hints.ai_flags = AI_PASSIVE;
+
+	if ((error = getaddrinfo(NULL, "2000", &hints, &res))<0) gai_strerror(error);
+
+	if (( sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol) ) <0 ) {
+		perror("error socket"); 
+		exit(2);
+	}
+
+	if ( bind(sockfd, res->ai_addr, res->ai_addrlen) < 0  ){
+		perror("error socket"); 
+		exit(2);
+	}
+
+	if (listen(sockfd, 10) < 0) {
+		perror("error socket"); 
+		exit(2);
+	}
+
+	if (( confd=accept(sockfd, NULL, NULL)) < 0) {
+		perror("error socket"); 
+		exit(2);
+	}
+
+	int nbOctets; 
+	char buffer [1024];
+
+	while ((nbOctets = recv(confd, buffer, sizeof(buffer), 0))>0){
+		if (write(1, buffer, nbOctets) < 0){
+			perror("error socket"); 
+			exit(2);
+		}
+	}
+
+	if (nbOctets <0) perror("error recv"); 
+	close(confd); 
+	close(sockfd);
+	freeaddrinfo(res);
+
+
+	return 0; 
+	
+
+}
