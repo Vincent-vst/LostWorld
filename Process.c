0931d27a53024df740ef62cab93b4fd118a25d2d
Vincent-vst <louise.insomnia@gmail.com>
2022-03-15 17:44:11 +0100

    comment process

--- Process.c
+++ Process.c
@@ -1,4 +1,3 @@
-//
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
@@ -14,7 +13,6 @@
 #include <fcntl.h>
 #include <stddef.h>
 #include <errno.h>
-/* #include "Parser.c" */
 
 int parseRequest(char*, int, char*, int);
 void traitementReponse(char*, int, int);
@@ -23,17 +21,14 @@ void traitementErreurOpen(int, int);
 void afficheIp(unsigned char* );
 
 
-void traitementRequete(int confd)
-{
+void traitementRequete(int confd) {
 	int nbOctets, fdFichier;
 	char buffer[1000],  fileName[100];
 	
 	char error400[]="HTTP/1.1 400 BAD REQUEST\r\nContent-Type: text/html\r\n\r\n";
-	if(recv(confd, buffer, sizeof(buffer)*sizeof(char), 0)<0) perror (" Erreur recv ");//Ici on reçoit la requête du client
+	if(recv(confd, buffer, sizeof(buffer)*sizeof(char), 0)<0) perror (" Erreur recv ");
 	
-	if(parseRequest(buffer, sizeof(buffer) , fileName, sizeof(fileName)) == -1 ) //On parse la requête afin d'en récuperer le nom du fichier qui doit être ouvert et on le met dans fileName
-	//Si la requête est incorrecte on renvoie dans ce bloc pour afficher l'erreur 400
-	{
+	if(parseRequest(buffer, sizeof(buffer) , fileName, sizeof(fileName)) == -1 ) {
 		if(send(confd, error400, sizeof(char)*strlen(error400) , 0)<0) perror (" Erreur send ");
 		if((fdFichier = open("website/file400.html", O_RDONLY))<0)perror("Erreur open");
 		
@@ -41,45 +36,38 @@ void traitementRequete(int confd)
 			if(send(confd, buffer, nbOctets , 0)<0) perror (" Erreur send ");
 		}			
 	} 
-	//Sinon on continue et on renvoie dans ce bloc pour ouvrir le fichier
-	else{		
-		if((fdFichier = open(fileName, O_RDONLY))<0)//On ouvre le fichier si il y a une erreur on renvoie dans la fonction traitementErreurOpen sinon on traite la Reponse
-		{
+
+	else {		
+		if((fdFichier = open(fileName, O_RDONLY))<0){
 			traitementErreurOpen(confd, fdFichier);
 		}
-		else{
+		else {
 			traitementReponse(fileName, confd, fdFichier);
 		}
 	}
 	close(confd);
 }
 
-
-//Cette fonction est utilisé quand le fichier à été ouvert avec succès, alors on renvoie l'en-tête http correspondant au type du fichier puis on affiche le contenu du fichier
 void traitementReponse(char* fileName, int confd, int fdFichier) {
 	int nbOctets = 0;
 	char buffer[1000];
 	char reponseHtml[] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
 	char reponseJpg[] = "HTTP/1.1 200 OK\r\nContent-Type: image/jpg\r\n\r\n";
 	char reponsePng[] = "HTTP/1.1 200 OK\r\nContent-Type: image/png\r\n\r\n";
-	if(strstr(fileName, ".html"))//Si c'est un fichier html
-	{
+	if(strstr(fileName, ".html")) {
 		if(send(confd, reponseHtml, sizeof(char)*strlen(reponseHtml) , 0)<0) perror (" Erreur send ");
 		
 		while((nbOctets = read(fdFichier, buffer, 100))>0){
 			if(send(confd, buffer, nbOctets , 0)<0) perror (" Erreur send ");
 		}
 	}
-	else if (strstr(fileName, ".png"))//Si c'est un fichier png
-	{
+	else if (strstr(fileName, ".png")) {
 		if(send(confd, reponsePng, sizeof(char)*strlen(reponsePng) , 0)<0) perror (" Erreur send ");
 		
 		while((nbOctets = read(fdFichier, buffer, 100))>0){
 			if(send(confd, buffer, nbOctets , 0)<0) perror (" Erreur send ");
 		}
-	}
-	else if (strstr(fileName, ".jpg"))//Si c'est un fichier jpeg
-	{
+	} else if (strstr(fileName, ".jpg")) {
 		if(send(confd, reponseJpg, sizeof(char)*strlen(reponseJpg) , 0)<0) perror (" Erreur send ");
 	
 		while((nbOctets = read(fdFichier, buffer, 100))>0){
